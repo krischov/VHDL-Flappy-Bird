b238f8906165ae52c87e2e2b8328d5da505b5df5
Ankush Patel <apat819@aucklanduni.ac.nz>
2021-05-04 13:07:05 +1200

    Created Quartus Project

---
+++ src/main.vhdl
@@ -0,0 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity main is
+end entity;
+
+architecture x of main is 
+
+begin
+
+end architecture;
--- src/vga_sync.vhd
+++ src/vga_sync.vhd
@@ -1,4 +1,3 @@
-
 library IEEE;
 use  IEEE.STD_LOGIC_1164.all;
 use  IEEE.STD_LOGIC_ARITH.all;
