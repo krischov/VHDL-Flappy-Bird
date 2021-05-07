fb102fd46ed535e114bf837e2dec1113f16429ea
Ankush <ankushjp4@gmail.com>
2021-05-07 12:33:01 +1200

    Added counter that counts from 0 -> 63

--- src/main.vhdl
+++ src/main.vhdl
@@ -27,10 +27,10 @@ architecture x of main is
 	
 	signal mouse_btn : string(1 to 50) := var_len_str("No Mouse Button Pressed", 50);
 	
-	signal num_test : integer range 1 to 50 := 35;
+	signal num_test : integer range 1 to 50 := 50;
 begin
 	textengine0: textengine port map(clk, text_vector, vga_row, vga_col, txt_r, txt_g, txt_b, txt_not_a);
-
+	
 	str2text(text_vector, 0, 0, 1, "1111", "1111", "1111", "ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789 []!@#$%""()&*+-,./");
 	str2text(text_vector, 1, 0, 1, "1111", "0000", "0000", ".........1.........2.........3.........4.........5.........6.........7.........8");
 	str2text(text_vector, 59, 0, 1, "1111", "0000", "0000", ".........1.........2.........3.........4.........5.........6.........7.........8");
@@ -40,34 +40,45 @@ begin
 	str2text(text_vector, 13, 20, 1, "0011", "1100", "1001", int2str(num_test));
 
 	process(clk)
+		variable ticks : integer := 0;
 	begin
-		-- change background colour
-		if (mouse_lbtn = '1') then
-			colour <= "0101";
-			mouse_btn <= var_len_str("Left Mouse button Pressed", mouse_btn'length);
-		elsif (mouse_rbtn = '1') then
-			colour <= "1010";
-			mouse_btn <= var_len_str("Right Mouse button Pressed", mouse_btn'length);
-		else
-			colour <= "1111";
-			mouse_btn <= var_len_str("No Mouse button Pressed", mouse_btn'length);
-		end if;
-		
-		-- draw text
-		if (txt_not_a = "1111") then
-			red_out <= txt_r;
-			green_out <= txt_g;
-			blue_out <= txt_b;
-		-- draw red square
-		elsif (vga_row < y0 + h and vga_row > y0 and 
-				vga_col < x0 + w and vga_col > x0) then
-			red_out <= colour;
-			green_out <= "0000";
-			blue_out <= "0000";
-		else
-			red_out <= "0000";
-			green_out <= "0000";
-			blue_out <= "0000";
+		if (rising_edge(clk)) then
+			ticks := ticks + 1;
+			if (ticks >= 25000000) then
+				num_test <= num_test + 1;
+				ticks := 0;
+			end if;
+			
+			-- change background colour
+			if (mouse_lbtn = '1') then
+				colour <= "0101";
+				mouse_btn <= var_len_str("Left Mouse button Pressed", mouse_btn'length);
+			elsif (mouse_rbtn = '1') then
+				colour <= "1010";
+				mouse_btn <= var_len_str("Right Mouse button Pressed", mouse_btn'length);
+			else
+				colour <= "1111";
+				mouse_btn <= var_len_str("No Mouse button Pressed", mouse_btn'length);
+			end if;
+			
+			
+			
+			-- draw text
+			if (txt_not_a = "1111") then
+				red_out <= txt_r;
+				green_out <= txt_g;
+				blue_out <= txt_b;
+			-- draw red square
+			elsif (vga_row < y0 + h and vga_row > y0 and 
+					vga_col < x0 + w and vga_col > x0) then
+				red_out <= colour;
+				green_out <= "0000";
+				blue_out <= "0000";
+			else
+				red_out <= "0000";
+				green_out <= "0000";
+				blue_out <= "0000";
+			end if;
 		end if;
 	end process;
 end architecture;
