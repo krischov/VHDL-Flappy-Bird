3a76a144ae37b6d09edc72d9bad937bb0ac405bc
uoa-kcho598 <kcho598@aucklanduni.ac.nz>
2021-05-08 23:12:57 +1200

    Implemented ColumnRow Component
    - Added pin assignments.
    - Gave VHDL code more descriptive names.
    - Connected block diagram.

--- src/ColumnRowComponent.vhd
+++ src/ColumnRowComponent.vhd
@@ -7,29 +7,29 @@ library lib;
 use lib.textengine_package.all;
 
 entity ColumnRowProcessor is
-  port(dpSwitch, Clk: in std_logic;
-    x_row, y_column: in unsigned(7 downto 0);
-    D3F, D2F, D1F, D0F: out unsigned(7 downto 0));
+  port(ToggleButtonXY, Clk: in std_logic;
+    x_row, y_column: in unsigned(9 downto 0);
+    Thousands, Hundreds, Tens, Ones: out unsigned(7 downto 0));
 end entity ColumnRowProcessor;
 
 architecture a1 of ColumnRowProcessor is
 begin
   process(Clk)
-    variable d3, d2, d1, d0: unsigned(7 downto 0);
+    variable thou, hund, ten, one: unsigned(7 downto 0);
     begin
       if(rising_edge(Clk)) then
-         if(dpSwitch = '0') then
-				int2bcd(x_row, d3, d2, d1, d0);
-				D3F <= d3;
-				D2F <= d2;
-				D1F <= d1;
-				D0F <= d0;
+         if(ToggleButtonXY= '0') then
+				int2bcd(resize(x_row, 13), thou, hund, ten, one);
+				Thousands <= thou;
+				Hundreds <= hund;
+				Tens <= ten;
+				Ones <= one;
 			else 
-				int2bcd(y_column, d3, d2, d1, d0);
-				D3F <= d3;
-				D2F <= d2;
-				D1F <= d1;
-				D0F <= d0;
+				int2bcd(resize(y_column, 13), thou, hund, ten, one);
+				Thousands <= thou;
+				Hundreds <= hund;
+				Tens <= ten;
+				Ones <= one;
 			end if;
 		end if;
     end process;
