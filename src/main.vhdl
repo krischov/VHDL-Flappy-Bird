6b1c24fdfc8444d0a411727b4d2430a30ae05345
Ankush <ankushjp4@gmail.com>
2021-05-24 02:55:36 +1200

    Merged

--- src/main.vhdl
+++ src/main.vhdl
@@ -372,7 +372,7 @@ begin
 								end if;
 							end if;
 						end if;
-
+						
 				-- if the user has just passed through this pipe, give them a point
 					if (bottompipe(i).passed_pipe = false and bird(0).x0 > bottompipe(i).x0 + bottompipe(i).size * bottompipe(i).scaling_factor_x) then
 						bottompipe(i).passed_pipe <= true;
