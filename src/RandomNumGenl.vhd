df49e35f2784af06cdcae3c227b5d11e2fafedf6
uoa-kcho598 <kcho598@aucklanduni.ac.nz>
2021-05-26 03:08:19 +1200

    Updated RN generator
    Increased to 10 bit,
    Added arbitrary maths to increase randomness

--- src/RandomNumGenl.vhd
+++ src/RandomNumGenl.vhd
@@ -3,44 +3,43 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity randomNumGen is
-	generic (seed: NATURAL range 1 to 13);
 	port(
 		clk							: 		in std_logic;
-		randNumX0  					: 		out std_logic_vector(8 downto 0);
-		randNumYScaleMultiplier	:		out std_logic_vector(3 downto 0)
+		seed          :   in natural range 1 to 1023;
+		randNum  					: 		out std_logic_vector(3 downto 0)
 	);
 end entity randomNumGen;
 
 architecture a of randomNumGen is
-	signal temp : std_logic_vector(8 downto 0) := std_logic_vector(to_unsigned(seed, 9));
+	signal temp : std_logic_vector(9 downto 0) := std_logic_vector(to_unsigned(seed, 10));
 begin
 	process (clk)
-	variable t_randNumYScale : std_logic_vector(3 downto 0);
+	variable t_upper      : integer range 0 to 9;
+	variable finalRandNum : std_logic_vector(3 downto 0);
+	
 	begin
-	if(rising_edge(clk)) then 
-	--Galois LFSR 9 bit
-	--ten bit needs(taps at 2, 9) for max range
-		temp(0) <= temp(8); --tap (8)
+	if(rising_edge(clk)) then
+		temp(0) <= temp(9); 
 		temp(1) <= temp(0);
 		temp(2) <= temp(1);
-		temp(3) <= temp(2);
-		temp(4) <= temp(3) xor temp(8); --tap (3)
+		temp(3) <= temp(2) xor temp(9);
+		temp(4) <= temp(3);
 		temp(5) <= temp(4);
 		temp(6) <= temp(5);
 		temp(7) <= temp(6);
 		temp(8) <= temp(7);
-		
-		t_RandNumYScale := temp(3 downto 0);
-		
-		if (t_RandNumYScale > "1000") then
-			t_RandNumYScale := std_logic_vector(shift_right(unsigned(t_RandNumYScale), 1));
-		elsif (t_RandNumYScale) = "0000" then
-			t_RandNumYScale := "0001";
-		end if;
+		temp(9) <= temp(8);
+		 
+		t_upper := to_integer(unsigned(temp(2 downto 0)));
+		if(t_upper <= 2) then
+      finalRandNum := temp(t_upper) & temp(t_upper + 3) & temp(t_upper + 4) & temp(t_upper + 2);
+    else
+      finalRandNum := temp(t_upper downto t_upper - 3);
+	  end if;
+	  
+	 randNum <= finalRandNum;
 	end if;
-	
-	randNumX0 <= temp;
-	randNumYScaleMultiplier <= t_RandNumYScale;
+
 	end process;	
 end architecture;
 		
